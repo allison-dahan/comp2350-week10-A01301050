cf74e444f5b0f960a196515ac7c7cbf8db524245
Allison Dahan <allison.dahan@yahoo.ca>
2024-03-27 14:16:37 -0700

    showPets

--- models/pet.js
+++ models/pet.js
@@ -1,24 +1,46 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const databaseConnectionString = include("/databaseConnectionSequelize");
+const userModel = include("models/web_user");
 const sequelize = new Sequelize(databaseConnectionString);
 const petModel = sequelize.define(
-    "pet",
-    {
-      pet_id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
-      },
-      web_user_id: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'web_user', key: 'web_user_id' } },
-      name: { type: Sequelize.STRING, allowNull: false },
-      pet_type_id: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'pet_type', key: 'pet_type_id' } },
+  "pet",
+  {
+    pet_id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
     },
-    {
-      tableName: "pet",
-      timestamps: false,
-      singular: "pet",
-      plural: "pet",
-    }
-  );
-  module.exports = petModel;
+    web_user_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: { model: "web_user", key: "web_user_id" },
+    },
+    name: { type: Sequelize.STRING, allowNull: false },
+    pet_type_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: { model: "pet_type", key: "pet_type_id" },
+    },
+  },
+  {
+    tableName: "pet",
+    timestamps: false,
+    singular: "pet",
+    plural: "pet",
+  }
+);
+
+petModel.belongsTo(userModel, {
+  as: "owner",
+  timestamps: false,
+  foreignKey: "web_user_id",
+});
+userModel.hasMany(petModel, {
+  as: "pets",
+  timestamps: false,
+  foreignKey: "web_user_id",
+});
+
+
+module.exports = petModel;
