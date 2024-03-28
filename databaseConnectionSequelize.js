3828c457b34e006a7fdda3e1228ecb2fe1606b5a
Allison Dahan <allison.dahan@yahoo.ca>
2024-03-27 22:33:02 -0700

    fix order

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,7 +1,7 @@
 const is_qoddi = process.env.IS_QODDI || false;
 const dbConfigQoddi =
   "mysql://freedb_2351_main:qE%6DmaDpeKvTpj@sql.freedb.tech:3306/freedb_comp2350-week2-A01301050";
-
+  
 // ? has to be encoded as %3F
 
 const dbConfigLocal = "mysql://root:Peachykeen23%3F@localhost:3306/week10"; 
